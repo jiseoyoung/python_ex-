ec9f6099d5f21252eabaeb2d7333a879601d0676
jiseoyoung <jiseoyoung@naver.com>
2018-01-15 15:24:05 +0900

    2회
    2회

---
+++ 2회/이름,전화번호,주소.py
@@ -0,0 +1,13 @@
+﻿grade =int(input("정수를 입력하세요"))
+if grade>=90:
+    print("당신의 학점은 A입니다")
+elif grade>=80:
+    print("당신의 학점은 B입니다")
+elif grade>=70:
+    print("당신의 학점은 C입니다")
+elif grade>=60:
+    print("당신의 학점은 D입니다")
+else:
+    print("당신의 학점은 F입니다")
+    
+    
